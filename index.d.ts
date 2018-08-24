68137b608215019bccb507ebb19b220d8c6a0014
hiko1129 <masaki_hikosaka@outlook.com>
2018-08-24 15:50:07 +0900

    added radio button group type

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,19 @@
+import { 
+  SelectFieldProps,
+  TextFieldProps,
+  RadioButtonGroupProps
+} from 'material-ui';
+import { WrappedFieldProps } from 'redux-form';
+import { Component, InputHTMLAttributes } from 'react';
 
 declare module 'redux-form-material-ui' {
-  import { SelectFieldProps } from 'material-ui';
-  import { TextFieldProps } from 'material-ui';
-  import { WrappedFieldProps } from 'redux-form';
-  import { Component, InputHTMLAttributes } from 'react';
-   
   export class SelectField
     extends Component<SelectFieldProps & WrappedFieldProps & InputHTMLAttributes<HTMLInputElement>> {}
   
   export class TextField
     extends Component<TextFieldProps & WrappedFieldProps & InputHTMLAttributes<HTMLInputElement>> {}
+
+  export class RadioButtonGroup
+    extends Component<RadioButtonGroupProps & WrappedFieldProps & InputHTMLAttributes<HTMLInputElement>> {}
+
 }
