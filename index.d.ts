28e994c2e1bd3adb70a6e9f3b0dc5cb9f484ed3d
Eiryyy <t.kawagishi@creativehope.co.jp>
2018-08-30 18:57:16 +0900

    Add Checkbox Type

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,8 @@
-import { 
+import {
   SelectFieldProps,
   TextFieldProps,
-  RadioButtonGroupProps
+  RadioButtonGroupProps,
+  CheckboxProps
 } from 'material-ui';
 import { WrappedFieldProps } from 'redux-form';
 import { Component, InputHTMLAttributes } from 'react';
@@ -9,11 +10,14 @@ import { Component, InputHTMLAttributes } from 'react';
 declare module 'redux-form-material-ui' {
   export class SelectField
     extends Component<SelectFieldProps & WrappedFieldProps & InputHTMLAttributes<HTMLInputElement>> {}
-  
+
   export class TextField
     extends Component<TextFieldProps & WrappedFieldProps & InputHTMLAttributes<HTMLInputElement>> {}
 
   export class RadioButtonGroup
     extends Component<RadioButtonGroupProps & WrappedFieldProps & InputHTMLAttributes<HTMLInputElement>> {}
+
+  export class Checkbox
+    extends Component<CheckboxProps & WrappedFieldProps & InputHTMLAttributes<HTMLInputElement>> {}
 
 }
